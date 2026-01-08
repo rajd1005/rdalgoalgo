56b057ea78bd1a54cd7f50e4adb6f7bb16f42b03
rajd1005 <rajdey.dey1@gmail.com>
2026-01-08 20:32:29 +0530

    Update strategy_manager.py

--- strategy_manager.py
+++ strategy_manager.py
@@ -225,7 +225,11 @@ def inject_simulated_trade(trade_data, is_active):
         save_trades(trades)
     else:
         # Trade completely finished in simulator
-        trade_data['pnl'] = round((trade_data['exit_price'] - trade_data['entry_price']) * trade_data['quantity'], 2)
+        if trade_data['status'] == "PENDING":
+             trade_data['pnl'] = 0
+        else:
+             trade_data['pnl'] = round((trade_data['exit_price'] - trade_data['entry_price']) * trade_data['quantity'], 2)
+        
         hist = load_history()
         hist.insert(0, trade_data)
         save_history_file(hist)
@@ -350,7 +354,7 @@ def update_risk_engine(kite):
                         t['targets_hit_indices'].append(i)
                         log_event(t, f"Target {i+1} Hit @ {tgt}")
                         
-                        # --- REMOVED T1 SL MOVE LOGIC HERE ---
+                        # --- TRAILING SL REMOVED ---
                         
                         if i == len(t['targets']) - 1:
                             exit_reason = "TARGET_HIT"

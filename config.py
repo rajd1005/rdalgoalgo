238d5b797273ac3878c1ebcd86b94b435ea6c896
rajd1005 <rajdey.dey1@gmail.com>
2026-01-11 22:40:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,6 @@ PORT = int(os.environ.get("PORT", 5000))
 DEFAULT_SL_POINTS = 20
 
 # Database Config
-# 1. Use Absolute Path for SQLite
 uri = os.getenv("DATABASE_URL", "sqlite:///" + os.path.join(basedir, "algo.db"))
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
@@ -27,7 +26,6 @@ if uri.startswith("postgres://"):
 SQLALCHEMY_DATABASE_URI = uri
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# 2. DB Setting: Enforce IST Timezone for PostgreSQL
 connect_args = {}
 if "postgresql" in uri:
     connect_args = {'options': '-c timezone=Asia/Kolkata'}

6f28e3b1353a4e3d9c30ed6f45e9f10d5e0ec0e6
rajd1005 <rajdey.dey1@gmail.com>
2026-01-13 03:58:58 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+# --- RESTORED AppSetting ---
 class AppSetting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.Text, nullable=False) # Stores JSON string
@@ -15,7 +16,6 @@ class TradeHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.Text, nullable=False) # Stores JSON string
 
-# --- NEW: PERMANENT NOTIFICATION TABLE ---
 class TradeNotification(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime)
